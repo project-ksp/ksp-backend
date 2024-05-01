aa391468123992731903bb333688b5c47cad2d30
Noverdi Ramadhan <64979175+noverdy@users.noreply.github.com>
2024-05-01 20:56:29 +0700

    feat: pagination on member

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -1,9 +1,23 @@
 import type { FastifyReply, FastifyRequest } from "fastify";
 import * as memberService from "@/services/member.service";
+import { z } from "zod";
+import { fromError } from "zod-validation-error";
 
-export async function index(_request: FastifyRequest, reply: FastifyReply) {
+export async function index(request: FastifyRequest, reply: FastifyReply) {
+  const validator = z.object({
+    page: z.preprocess(Number, z.number()).optional(),
+  });
+  const validated = validator.safeParse(request.query);
+  if (!validated.success) {
+    return reply.status(400).send({
+      message: fromError(validated.error).toString(),
+    });
+  }
+
+  const { page } = validated.data;
+
   reply.send({
     message: "Members successfully fetched.",
-    data: await memberService.getAllMembers(),
+    data: await memberService.getAllMembers(page),
   });
 }
--- src/services/member.service.ts
+++ src/services/member.service.ts
@@ -1,5 +1,8 @@
 import { db } from "@/db";
 
-export async function getAllMembers() {
-  return db.query.members.findMany();
+export async function getAllMembers(page = 1) {
+  return db.query.members.findMany({
+    limit: 10,
+    offset: (page - 1) * 10,
+  });
 }

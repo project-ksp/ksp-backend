8c03147cae474655587e3cc81e0bf0448be7224e
Noverdi Ramadhan <64979175+noverdy@users.noreply.github.com>
2024-05-02 12:28:10 +0700

    feature: pagination on members data (#7)
    * feat: pagination on member
    
    * feat: page numbers

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -1,9 +1,30 @@
 import type { FastifyReply, FastifyRequest } from "fastify";
 import * as memberService from "@/services/member.service";
+import { z } from "zod";
+import { fromError } from "zod-validation-error";
 
-export async function index(_request: FastifyRequest, reply: FastifyReply) {
-  reply.send({
-    message: "Members successfully fetched.",
-    data: await memberService.getAllMembers(),
+export async function index(request: FastifyRequest, reply: FastifyReply) {
+  const validator = z.object({
+    page: z.preprocess(Number, z.number()).optional(),
   });
+  const validated = validator.safeParse(request.query);
+  if (!validated.success) {
+    return reply.status(400).send({
+      message: fromError(validated.error).toString(),
+    });
+  }
+
+  const { page } = validated.data;
+
+  try {
+    const data = await memberService.getAllMembers(page);
+    reply.send({
+      message: "Members successfully fetched.",
+      data,
+    });
+  } catch (error) {
+    return reply.status(400).send({
+      message: error instanceof Error ? error.message : "An error occurred.",
+    });
+  }
 }

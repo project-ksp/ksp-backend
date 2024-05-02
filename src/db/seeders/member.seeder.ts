7ab44e0bb56799a46ef6c69acbe3ac8a261f65d1
Noverdi Ramadhan <64979175+noverdy@users.noreply.github.com>
2024-05-02 18:46:14 +0700

    chore: change seeder amount

--- src/db/seeders/member.seeder.ts
+++ src/db/seeders/member.seeder.ts
@@ -3,11 +3,11 @@ import memberFactory from "../factories/member.factory";
 import { members } from "../schemas/members.schema";
 
 export default async function seed() {
-  const memberData = await Promise.all(Array.from({ length: 50 }, memberFactory));
+  const memberData = await Promise.all(Array.from({ length: 1000 }, memberFactory));
   const branches = await db.query.branches.findMany();
   for (let i = 0; i < branches.length; i++) {
-    for (let j = 0; j < 5; j++) {
-      memberData[i * 5 + j]!.branchId = branches[i]!.id;
+    for (let j = 0; j < 100; j++) {
+      memberData[i * 100 + j]!.branchId = branches[i]!.id;
     }
   }
 

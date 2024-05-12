26d4159eb488ea1fcf4b8d0ace463c2413c4de03
Noverdi Ramadhan <64979175+noverdy@users.noreply.github.com>
2024-05-12 14:38:09 +0700

    chore: remove teller access on get all users

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -2,7 +2,7 @@ import * as userController from "@/controllers/user.controller";
 import type { FastifyInstance } from "fastify";
 
 const userRoutes = async (fastify: FastifyInstance) => {
-  fastify.get("/", { preHandler: [fastify.authenticate] }, userController.index);
+  fastify.get("/", { preHandler: [fastify.authorize(["owner", "branch_head"])] }, userController.index);
   fastify.post("/", { preHandler: [fastify.authorize(["owner"])] }, userController.create);
   fastify.get("/export", { preHandler: [fastify.authorize(["owner"])] }, userController.download);
 };

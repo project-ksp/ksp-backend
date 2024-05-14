6117cbe86b99e750aeabd5ad3045faab36990310
Noverdi Ramadhan <64979175+noverdy@users.noreply.github.com>
2024-05-14 09:25:42 +0700

    hotfix: fix issues after deployment (#11)

--- src/db/schemas/users.schema.ts
+++ src/db/schemas/users.schema.ts
@@ -25,7 +25,6 @@ export const usersRelations = relations(users, ({ one, many }) => ({
 }));
 
 export const userInsertSchema = createInsertSchema(users)
-  .strict()
   .omit({
     username: true,
     password: true,

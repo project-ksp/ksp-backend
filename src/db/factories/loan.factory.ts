e746cd377d856ca9dd502c87f2808a9d8ce84e69
kuhakuni <afif.haidar@gmail.com>
2024-05-30 01:32:36 +0700

    feat: Update loan factory to use ISO string format for start and end dates

--- src/db/factories/loan.factory.ts
+++ src/db/factories/loan.factory.ts
@@ -11,7 +11,7 @@ export default async function loanFactory(): Promise<typeof loans.$inferInsert> 
       max: 10000000,
     }),
     leaderId: "",
-    startDate: new Date(),
-    endDate,
+    startDate: new Date().toISOString(),
+    endDate: endDate.toISOString(),
   };
 }

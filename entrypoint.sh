0ecae8d0f4583b8b24c7faf0d4cc4bfff14dae9a
Evan <gebel909@gmail.com>
2024-05-29 22:08:32 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
 if [ "$ENV" = "sit" ]; then
+  echo -e "${YELLOW}Running in SIT mode${NC}"
   yarn migrate && yarn seed && yarn start
 elif [ "$ENV" = "prod" ]; then
+  echo -e "${YELLOW}Running in PROD mode${NC}"
   yarn start
 else
   echo "Unknown environment: $ENV"

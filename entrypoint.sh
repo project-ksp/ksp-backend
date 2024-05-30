235bcf6b0805a89e62442204002f2065ec06e04c
kuhakuni <afif.haidar@gmail.com>
2024-05-30 08:57:10 +0700

    refactor: Update entrypoint.sh to include clearing seed data before migration and starting the application

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ NC='\033[0m'
 
 if [ "$ENV" = "sit" ]; then
   echo -e "${YELLOW}Running in SIT mode${NC}"
-  yarn migrate && yarn seed && yarn start
+  yarn seed:clear && yarn migrate && yarn seed && yarn start
 elif [ "$ENV" = "prod" ]; then
   echo -e "${YELLOW}Running in PROD mode${NC}"
   yarn start
